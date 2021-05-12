ef3c4c39a6f9ffd3c5de441ae74ddf5448dc7cc1
Raghav-Lohani <76556271+Raghav-Lohani@users.noreply.github.com>
2021-05-12 14:33:11 +0530

    Add files via upload

---
+++ P-43-main/P-43-main/ProjectC-43-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    textSize(15)
+    fill ("white")
+    text("ARVIELB",100,50)
+    fill("lightblue")
+    text("hint;it is always  changing ,not changing",100,70)
+
+    textSize(15)
+    fill ("white")
+    text("bdaesta",100,250)
+    fill("lightblue")
+    text("hint; stores all information",100,270)
+
+    textSize(15)
+    fill ("white")
+    text("ucinfnt",700,150)
+    fill("lightblue")
+    text("hint;performs a particular task",700,170)
+    
+}
